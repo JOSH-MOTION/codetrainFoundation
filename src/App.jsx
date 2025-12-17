ac450df2d94e4daba3360a825a39b5bda9d42d03
JOSH-MOTION <joshuadoe168@gmail.com>
2025-12-17 15:13:22 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,21 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Home from './pages/Home'
-import PartnerPage from './pages/Partner'
-
-
-
+import Home from './pages/Home.jsx'
+import Partner from './pages/Partner.jsx'
 
 function App() {
-
   return (
-    <>
     <BrowserRouter>
-     <Navbar />
-    
-    
-    <Routes>
-    <Route path='/' element={ <Home />} />
-    <Route path='/partner' element={ <PartnerPage />} />
-  
-    </Routes>
-    <Footer />
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/partner" element={<Partner />} />
+      </Routes>
+
+      <Footer />
     </BrowserRouter>
-   
-    </>
   )
 }
 
