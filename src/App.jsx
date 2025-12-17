5dc2fb9c855108c81dd84f14b8a9c29eecaec9cc
JOSH-MOTION <joshuadoe168@gmail.com>
2025-12-17 14:36:03 +0000

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
-import PartnerPage from './pages/PartnerPage'
 import Footer from './components/Footer'
 import Home from './pages/Home'
-import MissionImpactSection from './pages/MissionSection'
+import PartnerPage from './pages/PartnerPage'
+
 
 
 
@@ -19,7 +19,7 @@ function App() {
     <Routes>
     <Route path='/' element={ <Home />} />
     <Route path='/partner' element={ <PartnerPage />} />
-    <Route path='/MissionImpact' element={ <MissionImpactSection />} />
+  
     </Routes>
     
     <Footer />
